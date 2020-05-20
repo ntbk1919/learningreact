e0ee9fe7a654ad39f3f26c172c5665bf55d86387
ntbk1919 <noeltbejoy@gmail.com>
2020-05-20 11:20:55 +0530

    First commit

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react';
+import Ninjas from'./ninjas';
+
+class App extends Component {
+ state ={
+  ninjas:[
+    {name:'RYU',age:30,belt:'black',id:1},
+    {name:'Yoshi',age:45,belt:'green',id:2},
+    { name:'Crystal',age:34,belt:'pink',id:3}
+  ]
+}
+   render(){
+  return (
+    <div className="App">
+      <h1>My first React App!</h1>
+      <p>Welcome xD</p>
+      <Ninjas ninjas={this.state.ninjas}/>
+      
+   
+    </div>
+  );
+  }
+
+}
+export default App;
